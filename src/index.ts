9c30525e2b69bdf084c35f5feb862be27073edd1
mridupragyadas-design <mridupragyadas@gmail.com>
2026-07-01 23:02:18 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ interface MessageTracker {
 }
 
 // -------------------- Configuration --------------------
-const BOT_TOKEN = process.env.SECURITY_BOT_TOKEN || '8970227707:AAE7gHha6huxmuSvfgIzCwOOgBXC6_GsOyw';
+const BOT_TOKEN = process.env.SECURITY_BOT_TOKEN || '8970227707:AAGfCVogN14Z3ex6WkABHAo0owRN32fOPFs';
 const DATA_FILE = 'security_bot_data.json';
 const DB_FILE = 'users.db';
 const HISTORY_FILE = 'user_history.json';
